3dd0ec483f8342fda878e1d862669c97737efd74
ritik-fj <113875093+ritik-fj@users.noreply.github.com>
2023-04-04 16:21:36 +1200

    deleted and created new policies controller/model

--- vt-insurance/app/Http/Controllers/PoliciesController.php
+++ vt-insurance/app/Http/Controllers/PoliciesController.php
@@ -2,9 +2,64 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Policies;
 use Illuminate\Http\Request;
 
 class PoliciesController extends Controller
 {
-    //
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Policies $policies)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Policies $policies)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Policies $policies)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Policies $policies)
+    {
+        //
+    }
 }
